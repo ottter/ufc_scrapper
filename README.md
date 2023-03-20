bba6bd0407535233a505fca671e87a712d2cfd69
james <me@jamescr.dev>
2023-03-20 15:22:51 -0400

    betting odds added

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Gather information about UFC events and fighters with BeautifulSoup4
 
 Print fighters from next MAIN card
 
-> `gather_next_event_info()[0]`
+> `gather_event_info(future_event=0)[0]`
 
 Print fighters from next PRELIM card
 
-> `gather_next_event_info()[1]`
+> `gather_event_info(future_event=0)[1]`
 
 Print all scheduled upcoming events in order
 
@@ -27,3 +27,15 @@ Print matchups from next PRELIM card
 Print ALL matchups from next event
 
 > `create_matchups(list(itertools.chain.from_iterable(gather_event_info(future_event=0))))`
+
+Print odds for next MAIN card
+
+> `create_odds_matchups(gather_odds_matchup(future_event=0)[0])`
+
+Print odds for next MAIN card and include favorite
+
+> `create_odds_matchups(gather_odds_matchups(mark_favorite=True)[0])`
+
+Print all odds for next card in ordered and unlabelled list
+
+> `gather_odds_matchups()`
--- main.py
+++ main.py
@@ -49,7 +49,10 @@ def gather_event_info(future_event=0):
     return list(dict.fromkeys(main_fighter_list)), list(dict.fromkeys(prelim_fighter_list))
 
 def create_matchups(card):
-    """Match the fighters with respective opponent (since scraper gets the fighter names and not matchups)"""
+    """Match the fighters with respective opponent (since scraper gets the fighter names and not matchups)    
+    
+    :param str card: 
+    """
     if len(card) % 2 != 0:
         return "Error: Official website has matchups out of order. Check again later"
     matchups = []
@@ -59,5 +62,39 @@ def create_matchups(card):
         i = i + 2
     return matchups
 
+def gather_odds_matchups(future_event=0, mark_favorite=False):
+    """Create list of odds for upcoming event, ordered and unlabeled.
+    
+    :param int future_event: X events in future to look up, default 0. Max range varies
+    :param bool mark_favorite: Choose whether to add betting odds favorite indicator
+    """
+    main_odds_list = []
+    prelim_odds_list = []
+    soup = BeautifulSoup(build_next_card_url(future_event).content, features="html.parser")
+    for x in [[main_odds_list, 'main-card'], [prelim_odds_list, 'prelims-card']]:
+        # NOTE: Works only for soonest upcoming event. Future events use different format: div class="l-main"
+        for matchup in soup.find_all('div', {'id': x[1]}):
+            for matchup in matchup.find_all('span', {'class': "c-listing-fight__odds-amount"}):
+                is_favorite = ''
+                if mark_favorite and "-" in matchup.text and len(matchup.text) > 1:
+                    is_favorite = " (Favorite)"
+                x[0].append(matchup.text + is_favorite)
+    return main_odds_list, prelim_odds_list
+
+def create_odds_matchups(card):
+    """Match the fighter's odds with respective opponent
+    
+    :param str card: 
+    """
+    if len(card) % 2 != 0:
+        return "Error: Official website has matchups out of order. Check again later"
+    matchups = []
+    i = 0
+    for x in range(len(card) // 2):
+        matchups.append(f"{card[i]} [odds] {card[i+1]}")
+        i = i + 2
+    return matchups
+
 #### Print matchups from next MAIN card
-print('main: ', create_matchups(gather_event_info(future_event=0)[0]))
+print(create_matchups(gather_event_info()[0]))
+print(create_odds_matchups(gather_odds_matchups()[0]))
